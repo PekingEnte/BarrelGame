608b50e20528dddfa4813870cb8c4d801bf34f69
PekingEnte <jessica.boerner@btk-fh.de>
2015-12-01 15:30:43 +0100

    Ursprüngliches BarrelGame

---
+++ BoxGame/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour {
+	
+	public int points;
+	public Text pointsText;
+	
+	
+	void Update()
+	{
+		pointsText.text = ("Points: " + points);
+	}
+}
